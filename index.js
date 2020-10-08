4c0bfd6440e1f914e1db69269c6fd9d4816d2e1e
Eric Lee <ranye0719@gmail.com>
2020-10-08 15:42:30 +0800

    using electron ipc to communicate between the main browser and the app

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    mainWindow.webContents.send('video:duration', metadata.format.duration);
+  })
 });
